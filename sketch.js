978c6161100579cc29ff9db727161e4c847af2a3
insuas <33041343+insuas@users.noreply.github.com>
2019-05-24 17:08:24 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function draw() {
   background(255);
   rectMode(CENTER);
   translate(width / 2, height / 2);
-  translate(p5.Vector.fromAngle(millis() / 1000, 40));
+  translate(p5.Vector.fromAngle(millis() / 1000, 550));
   circle(0, 0, 50);
 }
   
