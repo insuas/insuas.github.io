622df87d0518f754c21df725d03b91585e7d2ad2
Valeri Insuasti Caballero <v.insuasti15@ciaf.edu.co>
2019-05-29 01:17:34 -0500

    o

--- sketch.js
+++ sketch.js
@@ -1,14 +1,33 @@
+
 function setup(){
 createCanvas(600, 600);
+
+
 }
 
 
 function draw() {
-  background(255);
-  color('#123456');
+  background(51);
   rectMode(CENTER);
   translate(width / 2, height / 2);
   translate(p5.Vector.fromAngle(millis() / 1000, 275));
-  circle(0, 0, 50);
+  fill(random(255),random(255), random(255));
+  circle(0, 0, 20);
+
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      
+      
+      
+    }
+  }
+  //color('#123456');
+ // rectMode(CENTER);
+  //translate(width / 2, height / 2);
+  //translate(p5.Vector.fromAngle(millis() / 1000, 275));
+  //fill(random(255),random(255),random(255));
+  //circle(0, 0, 50);
+  
+ updatePixels();
 }
   
